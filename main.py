a6818c4741e25ca2e6bbc290eed1bea1ae9b50dd
Junaid Alam <15498725-JunaidAlam6@users.noreply.replit.com>
2024-07-29 20:43:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+
+from sys import maxsize
+
+def createStack():
+    stack = []
+    return stack
+
+def isEmpty(stack):
+    return len(strack) == 0
+
+def push(stack, item):
+    stack.append(item)
+
+def pop(stack):
+    if(isEmpty(stack)):
+        return 'stack is already empty'
+
+    return stack.pop()
+
+def peek(stack):
+    if(isEmpty(stack)):
+        return 'stack is empty'
+
+    return stack[len(stack) - 1]
+
+
+stack = createStack()
+push(stack, str(10))
+push(stack, str(20))
+push(stack, str(30))
+print(stack)

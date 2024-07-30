c27204a87aaa753696a22db2a8a8f71ffc2aab60
Junaid Alam <110741786+junaidalam2@users.noreply.github.com>
2024-07-30 17:14:39 +0000

    working on daily temperatures

--- leetcode/1_daily_temperature.py
+++ leetcode/1_daily_temperature.py
@@ -44,4 +44,19 @@ def dailyTemperature(temperatures):
 
 print(dailyTemperature(temperatures))
 
-      
+
+
+# solution
+
+class Solution(object):
+  def dailyTemperatures(self, temperatures):
+    n = len(temperatures)
+    ans = [0 for _ in range(n)]
+    st = []
+    for i in range(0, n):
+        while (len(st)>0) and temperatures[i]> temperatures[st[len(st)-1]]:
+            index = st[len(st)-1]
+            st.pop()
+            ans[index] = i-index
+        st.append(i)
+    return list(ans)
